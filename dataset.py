b23552b98104075298621365d7e4791af4101837
mahdi-khoshmaram <mahdikhoshmaramzade@gmail.com>
2024-05-30 15:22:13 +0330

    dataset.py updated.

--- dataset.py
+++ dataset.py
@@ -20,41 +20,96 @@ with open(name) as fh:
 
 documents = soup.find_all('document')
 
+
 for doc in documents:
+    # for storing id, title, abstract, relations
+    jdoc = {}
+
+    # for storing mesh:{type, name}
     anno_dic = {}
+
+    # for storing relations of one document
+    jrel = []
+
     # set id
     id = doc.id.string
+    jdoc['id'] = id
     
     # parse passages
     passages = doc.find_all('passage')
+    # parse relations
+    relations = doc.find_all('relation')
 
-    # write .PubTator.txt  file
-    with open('train.PubTator.txt', "a") as fh:
-        for psg in passages:
-            # type = title or abstract
-            type = psg.infon.string
-            
-            # title
-            if type == 'title':
-                text = psg.find('text', recursive = False).string
-                # fh.write(f"{id}|t|{text}")
-            
-            # abstract
-            if type == 'abstract':
-                text = psg.find('text', recursive = False).string
-                # fh.write(f"{text}\n\n")
+    # passage
+    for psg in passages:
+        # type = title or abstract
+        type = psg.infon.string
+        
+        # title
+        if type == 'title':
+            text = psg.find('text', recursive = False).string
+            jdoc['title'] = text
+        
+        # abstract
+        if type == 'abstract':
+            text = psg.find('text', recursive = False).string
+            jdoc['abstract'] = text
 
-            # entities
-            annotations = psg.find_all("annotation", recursive = False)
-            for anno in annotations:
-                mesh = anno.find(attrs={"key": "MESH"}).string
-                en_type = anno.find(attrs={"key": "type"}).string
-                en_name = anno.find("text", recursive = False).string
-                
-                if mesh in anno_dic.keys():
-                    continue
-                print(children(anno))
-                anno_dic[mesh] = {'type':en_type, 'name':en_name}
+        # entities
+        annotations = psg.find_all("annotation", recursive = False)
+        for anno in annotations:
+            Multi = False
+            print(anno.find(attrs = {"key":"CompositeRole"}, recursive = False))
+            if anno.find(attrs = {"key":"CompositeRole"}, recursive = False).string == 'CompositeMention':
+                continue
+            
+            mesh = anno.find(attrs={"key": "MESH"}, recursive = False).string
+            en_type = anno.find(attrs={"key": "type"}, recursive = False).string
+            en_name = anno.find("text", recursive = False).string
+
+            if "|" in mesh:
+                meshs = mesh.split('|')
+                Multi = True
+            
+            if Multi:
+                for mesh in meshs:
+                    if mesh in anno_dic.keys():
+                        continue
+                    anno_dic[mesh] = {'type':en_type, 'name':en_name}
+                break
+
+            if mesh in anno_dic.keys():
+                continue
+
+            anno_dic[mesh] = {'type':en_type, 'name':en_name}
+
+    # relation
+    for rel in relations:
+        rel_type = rel.find(attrs={"key": "relation"}, recursive = False).string
+        chemical_id = rel.find(attrs={"key": "Chemical"}, recursive = False).string
+        disease_id = rel.find(attrs={"key": "Disease"}, recursive = False).string
+
+        chemical = anno_dic[chemical_id]
+        disease = anno_dic[disease_id]
+
+        jrel.append({"rel_type":rel_type, "chemical":chemical, "disease":disease})
+
+    # # write .PubTator.txt  file
+    # with open('train.PubTator.txt', "a") as fh:
+    #     id = jdoc['id']
+    #     title = jdoc['title']
+    #     abstract = jdoc['abstract']
+    #     fh.write(f"{id}|t|{title} {abstract}")
+    #     fh.write
+    
+
+    
+    
+    
+    
+    
+    
+    
     # if id == '2234245':
     #     print(anno_dic)
 
